c7d14c8b55f7898274e44419e41796317dbadc7e
amj2900 <amj29@students.calvin.edu>
2020-11-27 20:14:13 -0600

    added health, fixed up actions and abilities

--- src/app/components/health/health.component.ts
+++ src/app/components/health/health.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-health',
@@ -9,7 +12,25 @@ export class HealthComponent implements OnInit {
 
   constructor() { }
 
+  hp: number = 30;
+  maxHP: number = 30;
+  inputHP: string;
+
   ngOnInit(): void {
+  }
+
+
+  gainHP(): void {
+    this.hp += 1
+  }
+
+  loseHP(): void {
+    this.hp -= 1
+  }
+
+  setHP(): void {
+    this.hp = Number(this.inputHP);
+    this.inputHP = '';
   }
 
 }
