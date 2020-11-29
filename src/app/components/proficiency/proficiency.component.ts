9419f11d0aae4f44dc36f65939ea2680d7b49b16
TheDunco <duncvan@gamil.com>
2020-11-29 13:04:46 -0500

    Added proficiency functionality

--- src/app/components/proficiency/proficiency.component.ts
+++ src/app/components/proficiency/proficiency.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CharacterService } from 'src/app/services/character.service';
 
 @Component({
   selector: 'app-proficiency',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProficiencyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private character: CharacterService) { }
 
-  ngOnInit(): void {
+  profBonus: number
+  selectedAbility = this.character.spellcastingAbility;
+  
+  // Spell save DC = 8 + proficiency bonus + spellcasting mod
+  spellSaveDC: number;
+  
+  // Spell attack = spellcasting mod + proficiency bonus
+  attackBonus: number;
+  
+  languages = this.character.languages
+  miscProfs = this.character.miscProfs
+  
+  update(): void {
+    this.profBonus = this.character.proficiencyBonus;
+    this.character.setSpellCastingAbility(this.selectedAbility)
+    
+    this.character.setLanguages(this.languages)
+    this.languages = this.character.languages
+    
+    this.character.setMiscProfs(this.miscProfs)
+    this.miscProfs = this.character.miscProfs
+    
+    // Spell save DC = 8 + proficiency bonus + spellcasting mod
+    this.spellSaveDC = this.character.proficiencyBonus + 8 + this.character.toMod(this.character.getAbilityScore(this.selectedAbility));
+    
+    // Spell attack = spellcasting mod + proficiency bonus
+    this.attackBonus = this.character.toMod(this.character.getAbilityScore(this.selectedAbility)) + this.character.proficiencyBonus;
+  }
+  ngOnInit(): void { this.update()
   }
 
 }
