92f68eb94499ecf31faa43c2a1b9902d402e0189
Kumar The Koala <haramkoo@gmail.com>
2020-11-29 18:29:11 -0400

    Merge branch 'note-dialog' of https://github.com/TheDunco/character-sheet into note-dialog

--- src/app/components/abilities/abilities.component.ts
+++ src/app/components/abilities/abilities.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {abilities, CharacterService} from '../../services/character.service';
+import { MatDialog, MatDialogModule, MatDialogConfig } from '@angular/material/dialog';
+import { AbilityDialogComponent } from '../ability-dialog/ability-dialog.component';
 
 @Component({
   selector: 'app-abilities',
@@ -9,14 +11,23 @@ import {abilities, CharacterService} from '../../services/character.service';
 export class AbilitiesComponent implements OnInit {
 
   abilityList: abilities[] = this.character.getAbilities();
-  name: string;
-  summary: string;
-  description: string;
 
-  constructor(private character: CharacterService ) { }
+  constructor(private character: CharacterService, private matDialog: MatDialog ) { }
 
+  openDialog(abilityName: string, abilitySummary: string) {
+    const dialogConfig = new MatDialogConfig()
+    this.matDialog.open(AbilityDialogComponent, {
+
+      width: '30vmax',
+      data: {
+        name: abilityName,
+        summary: abilitySummary
+      }
+
+    });
+    
+  }
   ngOnInit(): void {
   }
-
 
 }
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CharacterService } from 'src/app/services/character.service';
 
 @Component({
   selector: 'app-skills',
@@ -7,9 +8,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public character: CharacterService) { }
 
-  ngOnInit(): void {
+  passiveInvestigation: number
+  passivePerception: number
+  acrobatics: number
+  animalHandling: number
+  arcana: number
+  athletics: number
+  deception: number
+  history: number
+  insight: number
+  intimidation: number
+  investigation: number
+  medicine: number
+  nature: number
+  perception: number
+  performance: number
+  persuasion: number
+  religion: number
+  sleightOfHand: number
+  stealth: number
+  survival: number
+  
+  changeProf(name: string): void {
+    if (this.character.isProficient(name)) {
+      this.character.removeProficiency(name)
+    }
+    else {
+      this.character.addProficiency(name)
+    }
+    this.updateSkills();
+  }
+    
+  updateSkills(): void {
+    this.passiveInvestigation = this.character.toMod(this.character.abilityScores.intelligence) + 10;
+    this.passivePerception = this.character.toMod(this.character.abilityScores.wisdom) + 10;
+    this.acrobatics = this.character.toMod(this.character.abilityScores.dexterity) + (this.character.isProficient('acrobatics') ? this.character.proficiencyBonus : 0);
+    this.animalHandling = this.character.toMod(this.character.abilityScores.wisdom) + (this.character.isProficient('animalHandling') ? this.character.proficiencyBonus : 0);
+    this.arcana = this.character.toMod(this.character.abilityScores.intelligence) + (this.character.isProficient('arcana') ? this.character.proficiencyBonus : 0);
+    this.athletics = this.character.toMod(this.character.abilityScores.strength) + (this.character.isProficient('athletics') ? this.character.proficiencyBonus : 0);
+    this.deception = this.character.toMod(this.character.abilityScores.charisma) + (this.character.isProficient('deception') ? this.character.proficiencyBonus : 0);
+    this.history = this.character.toMod(this.character.abilityScores.intelligence) + (this.character.isProficient('history') ? this.character.proficiencyBonus : 0);
+    this.insight = this.character.toMod(this.character.abilityScores.wisdom) + (this.character.isProficient('insight') ? this.character.proficiencyBonus : 0);
+    this.intimidation = this.character.toMod(this.character.abilityScores.charisma) + (this.character.isProficient('intimidation') ? this.character.proficiencyBonus : 0);
+    this.investigation = this.character.toMod(this.character.abilityScores.intelligence) + (this.character.isProficient('investigation') ? this.character.proficiencyBonus : 0);
+    this.medicine = this.character.toMod(this.character.abilityScores.wisdom) + (this.character.isProficient('medicine') ? this.character.proficiencyBonus : 0);
+    this.nature = this.character.toMod(this.character.abilityScores.intelligence); + (this.character.isProficient('nature') ? this.character.proficiencyBonus : 0)
+    this.perception = this.character.toMod(this.character.abilityScores.wisdom) + (this.character.isProficient('perception') ? this.character.proficiencyBonus : 0);
+    this.performance = this.character.toMod(this.character.abilityScores.charisma) + (this.character.isProficient('performance') ? this.character.proficiencyBonus : 0);
+    this.persuasion = this.character.toMod(this.character.abilityScores.charisma) + (this.character.isProficient('persuasion') ? this.character.proficiencyBonus : 0);
+    this.religion = this.character.toMod(this.character.abilityScores.intelligence) + (this.character.isProficient('religion') ? this.character.proficiencyBonus : 0);
+    this.sleightOfHand = this.character.toMod(this.character.abilityScores.dexterity) + (this.character.isProficient('sleightOfHand') ? this.character.proficiencyBonus : 0);
+    this.stealth = this.character.toMod(this.character.abilityScores.dexterity) + (this.character.isProficient('stealth') ? this.character.proficiencyBonus : 0);
+    this.survival = this.character.toMod(this.character.abilityScores.wisdom) + (this.character.isProficient('survival') ? this.character.proficiencyBonus : 0);
+  }
+  
+  ngOnInit(): void { 
+    this.updateSkills();
   }
 
 }
