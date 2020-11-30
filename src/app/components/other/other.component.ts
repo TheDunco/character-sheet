0387002aa40c68096a6f1142416a9dad18edcc48
TheDunco <duncvan@gamil.com>
2020-11-30 14:11:57 -0500

    Tried to get long/short rest working, but can't get health to update correctly

--- src/app/components/other/other.component.ts
+++ src/app/components/other/other.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CharacterService } from 'src/app/services/character.service';
 
 @Component({
   selector: 'app-other',
@@ -7,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OtherComponent implements OnInit {
 
-  constructor() { }
+  constructor(private character: CharacterService) { }
 
+  longRest() {
+    this.character.health.hpCurrent = this.character.health.hpMax
+    console.log(this.character.health.hpCurrent)
+  }
+  
+  shortRest() {
+    
+  }
   ngOnInit(): void {
   }
 
