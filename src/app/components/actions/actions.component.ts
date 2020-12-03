4deefd8ecc6982346f98295b40b60baed2d0fe87
amj2900 <amj29@students.calvin.edu>
2020-12-03 12:53:03 -0600

    fixed what i goofed

--- src/app/components/actions/actions.component.ts
+++ src/app/components/actions/actions.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {action, CharacterService} from '../../services/character.service';
+
 
 @Component({
   selector: 'app-actions',
@@ -7,9 +9,36 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ActionsComponent implements OnInit {
 
-  @Input() name: string;
-  @Input() description: string;
-  constructor() { }
+  actionList: action[] = this.character.getActions();
+  hasMelee: boolean = false;
+  hasRange: boolean = false;
+  hasMagic: boolean = false;
+  hasPotion: boolean = false;
+  hasSpecial: boolean = false;
+  hasPower: boolean = false;
+  hasSpell: boolean = false;
+
+  constructor(private character: CharacterService) {
+
+    for (let i = 0; i < this.actionList.length; i++){
+      switch (this.actionList[i].actionType){
+        case "Magic Item": {this.hasMagic = true;
+          break;}
+        case "Melee": {this.hasMelee = true;
+          break;}
+        case "Potion": {this.hasPotion = true;
+          break;}
+        case "Power": {this.hasPower = true;
+          break;}
+        case "Range": {this.hasRange = true;
+          break;}
+        case "Special": {this.hasSpecial = true;
+          break;}
+        case "Spell": {this.hasSpell = true;
+          break;}
+      }
+    }
+   }
 
   ngOnInit(): void {
   }
