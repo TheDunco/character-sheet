cd51e979ccf1d070a89c5d8caebfdc3ba1c9d820
Kumar The Koala <haramkoo@gmail.com>
2020-12-04 14:00:04 -0400

    fixed delete bug

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -272,8 +272,8 @@ export class CharacterService {
     this.exampleNote.push(nData);
   }
 
-  updateNote(nData: note, oNote:string):void {
-    const index = this.exampleNote.findIndex(item => item.nTitle === oNote);
+  updateNote(nData: note, tNote:string, dNote:string):void {
+    const index = this.exampleNote.findIndex(item => item.nTitle === tNote && item.nDescription === dNote);
     if (index > -1) {
       this.exampleNote[index].nTitle = nData.nTitle;
       this.exampleNote[index].nDescription = nData.nDescription;
@@ -283,8 +283,8 @@ export class CharacterService {
     }
   }
 
-  deleteNote(oNote:string){
-    const index = this.exampleNote.findIndex(item => item.nTitle === oNote);
+  deleteNote(tNote:string, dNote:string){
+    const index = this.exampleNote.findIndex(item => item.nTitle === tNote && item.nDescription === dNote);
     if (index > -1) {
       // delete this.exampleNote[index];
       this.exampleNote.splice(index, 1);
