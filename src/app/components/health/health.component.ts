7a0cf9d4fb279132e5d1b852f06b89cd9158c8f6
TheDunco <duncvan@gamil.com>
2020-12-15 00:25:57 -0500

    Fixed health further and removed unecessary stuff

--- src/app/components/health/health.component.ts
+++ src/app/components/health/health.component.ts
@@ -8,81 +8,72 @@ import { CharacterService, Health } from '../../services/character.service'
 })
 export class HealthComponent implements OnInit {
 
-  constructor(private character: CharacterService) { }
+  constructor(public character: CharacterService) { }
   
   ngOnInit(): void {
     this.updateHealth();
   }
   
-  health: Health = this.character.getHealth();
-  hp = this.health.hpCurrent;
-  tempHP = this.health.hpTemp;
   inputHP: string;
   inputHealthDamage: number;
   healthPercent: number;
   progressColor = "primary";
 
   gainHP(): void {
-    if (this.hp + 1 <= this.health.hpMax) {
-      this.hp += 1
+    if (this.character.health.hpCurrent + 1 <= this.character.health.hpMax) {
+      this.character.health.hpCurrent += 1
       this.updateHealth();
     }
   }
 
   loseHP(): void {
-    if (this.tempHP > 0) {
-      this.tempHP -= 1;
+    if (this.character.health.hpTemp > 0) {
+      this.character.health.hpTemp -= 1;
     } else {
-      this.hp -= 1
+      this.character.health.hpCurrent -= 1
     }
     this.updateHealth();
   }
 
   setHP(): void {
-    this.hp = Number(this.inputHP);
+    this.character.health.hpCurrent = Number(this.inputHP);
     this.inputHP = '';
     this.updateHealth();
   }
   
   heal(): void {
-    if (this.hp + Number(this.inputHealthDamage) <= this.health.hpMax) {  
+    if (this.character.health.hpCurrent + Number(this.inputHealthDamage) <= this.character.health.hpMax) {  
       if (this.inputHealthDamage != undefined) {
-        this.hp += +this.inputHealthDamage;
+        this.character.health.hpCurrent += +this.inputHealthDamage;
       }
       this.updateHealth();
     } else {
-      this.hp = this.health.hpMax;
+      this.character.health.hpCurrent = this.character.health.hpMax;
     }
     this.updateHealth()
   }
   
   damage(): void {
     if (this.inputHealthDamage != undefined) {
-      if (this.tempHP - this.inputHealthDamage < 0) {
-        let rollover = this.inputHealthDamage - this.tempHP;
-        this.tempHP = 0;
-        this.hp -= rollover;
-      } else if (this.tempHP > 0) {
-        this.tempHP -= +this.inputHealthDamage;      
+      if (this.character.health.hpTemp - this.inputHealthDamage < 0) {
+        let rollover = this.inputHealthDamage - this.character.health.hpTemp;
+        this.character.health.hpTemp = 0;
+        this.character.health.hpCurrent -= rollover;
+      } else if (this.character.health.hpTemp > 0) {
+        this.character.health.hpTemp -= +this.inputHealthDamage;      
       } else {
-        this.hp -= +this.inputHealthDamage;
+        this.character.health.hpCurrent -= +this.inputHealthDamage;
       }
     }
     this.updateHealth();
   }
   
   updateHealth(): void {
-    this.health.hpCurrent = this.hp
-    this.character.setHealth(this.health)
-    this.healthPercent = (this.hp / this.health.hpMax) * 100
+    this.healthPercent = (this.character.health.hpCurrent / this.character.health.hpMax) * 100
     if (this.healthPercent <= 50) {
       this.progressColor = "warn"
     } else {
       this.progressColor = "primary"
     }
-  }
-  
-  refresh(): void {
-    this.health = this.character.health
   }
 }
